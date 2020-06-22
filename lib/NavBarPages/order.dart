428be194cc496edcf0229b0008692d31b18e53fa
dranzerAvi <aviralhtc2015@gmail.com>
2020-06-22 15:00:24 +0530

    Order a Report page UI with Search functionality🤘

--- lib/NavBarPages/order.dart
+++ lib/NavBarPages/order.dart
@@ -1,13 +1,268 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:getflutter/components/appbar/gf_appbar.dart';
+import 'package:moverzfax/Classes/mover.dart';
 
-class OrderPage extends StatefulWidget {
-  @override
-  _OrderPageState createState() => _OrderPageState();
-}
-
-class _OrderPageState extends State<OrderPage> {
+class OrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new Search());
+  }
+}
+
+class Search extends StatefulWidget {
+  Search({Key key}) : super(key: key);
+
+  @override
+  _SearchState createState() => new _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  TextEditingController editingController = TextEditingController();
+
+  List<String> duplicateItems;
+  var items = List<String>();
+
+  @override
+  void initState() {
+    fetchData();
+    duplicateItems =
+        List<String>.generate(data.length, (i) => data[i].moverName);
+    items.addAll(duplicateItems);
+    super.initState();
+  }
+
+  void filterSearchResults(String query) {
+    List<String> dummySearchList = List<String>();
+    dummySearchList.addAll(duplicateItems);
+    if (query.isNotEmpty) {
+      List<String> dummyListData = List<String>();
+      dummySearchList.forEach((item) {
+        if (item.contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        items.clear();
+        items.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        items.clear();
+        items.addAll(duplicateItems);
+      });
+    }
+  }
+
+  bool isLoaded = false;
+  List<Mover> data = new List();
+
+  List<Mover> parseMovers(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+    return parsed.map<Mover>((json) => Mover.fromJson(json)).toList();
+  }
+
+  fetchData() async {
+    if (isLoaded == false) {
+      var response = await http.get('http://localhost:27017/movers/listall');
+      setState(() {
+        data = parseMovers(response.body);
+        isLoaded = true;
+        print(data.length.toString());
+      });
+    }
+    List<String> temp = new List();
+    for (int i = 0; i < data.length; i++) {
+      temp.add(data[i].moverName);
+    }
+    duplicateItems = temp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    fetchData();
+
+    return new Scaffold(
+      appBar: GFAppBar(
+        backgroundColor: Color(0xFF3871AD),
+        title: Text(
+          "MoverZfax",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            fontFamily: 'nunito',
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  filterSearchResults(value);
+                },
+                controller: editingController,
+                decoration: InputDecoration(
+                    labelText: "Search",
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  print(items.length.toString());
+                  return InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
+                      child: Container(
+                        height: 218,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                spreadRadius: 3,
+                                blurRadius: 10,
+                              )
+                            ],
+                            color: Color(0xFF24408F),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 45,
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(15, 5, 0, 0),
+                                child: Text(
+                                  data[index].moverName,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'nunito',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                  overflow: TextOverflow.fade,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 120,
+                              child: Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      radius: 44,
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        'PhNo- ${data[index].moverPhno}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'nunito',
+                                            fontSize: 14),
+                                        overflow: TextOverflow.fade,
+                                      ),
+                                      Text(
+                                        'Rating- ${data[index].moverRating}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'nunito',
+                                            fontSize: 14),
+                                        overflow: TextOverflow.fade,
+                                      ),
+                                      Text(
+                                        'USDOT No- ${data[index].moverUSDOTNo}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'nunito',
+                                            fontSize: 14),
+                                        overflow: TextOverflow.fade,
+                                      ),
+                                      Text(
+                                        'MC No- ${data[index].moverMCNo}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'nunito',
+                                            fontSize: 14),
+                                        overflow: TextOverflow.fade,
+                                      ),
+                                      SizedBox(
+                                        height: 30,
+                                        width: 230,
+                                        child: Text(
+                                          'Description- ${data[index].moverDescription}',
+                                          softWrap: true,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'nunito',
+                                              fontSize: 14),
+                                          overflow: TextOverflow.fade,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              height: 50,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(15),
+                                  bottomRight: Radius.circular(15),
+                                ),
+                                color: Color(0xFF424242),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Order A report',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'nunito',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
