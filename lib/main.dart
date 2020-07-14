93a8a7255648efc9627d50aef2d6843fcb9f3622
scorchy38 <vkumarsaraswat@gmail.com>
2020-07-14 18:57:14 +0530

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moverzfax/OtherPages/SplashScreen.dart';
-import 'package:moverzfax/auth/signUp.dart';
 import 'package:persistent_bottom_nav_bar/models/persistent-bottom-nav-bar-styles.widget.dart';
 import 'package:persistent_bottom_nav_bar/models/persistent-nav-bar-scaffold.widget.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.widget.dart';
